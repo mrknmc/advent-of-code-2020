b92b2b92f2a050afb67f94c2d97d43ba8b37736c
Mark Nemec <mrknmc@me.com>
2020-12-05 13:10:27 +0100

    Add day 5

---
+++ day-05/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("data.txt")
+	fscanner := bufio.NewScanner(file)
+	maxSeatID := 0
+	seats := make([]int, 1024)
+
+	for fscanner.Scan() {
+		line := fscanner.Text()
+		rowHigh := 127
+		rowLow := 0
+
+		rowIndex := line[0:7]
+		for _, i := range rowIndex {
+			pivot := rowLow + (rowHigh+1-rowLow)/2
+			if i == 'F' {
+				rowHigh = pivot - 1
+			} else if i == 'B' {
+				rowLow = pivot
+			}
+		}
+		if rowLow != rowHigh {
+			panic("high and low should be equal")
+		}
+
+		colHigh := 7
+		colLow := 0
+		colIndex := line[7:10]
+		for _, i := range colIndex {
+			pivot := colLow + (colHigh+1-colLow)/2
+			if i == 'L' {
+				colHigh = pivot - 1
+			} else if i == 'R' {
+				colLow = pivot
+			}
+		}
+		if colLow != colHigh {
+			panic("high and low should be equal")
+		}
+		seatID := rowLow*8 + colLow
+		seats[seatID] = 1
+		if seatID > maxSeatID {
+			maxSeatID = seatID
+		}
+	}
+	fmt.Println("Max seat id: ", maxSeatID)
+
+	mySeatID := maxSeatID - 1
+	for {
+		if seats[mySeatID] == 0 && seats[mySeatID-1] == 1 && seats[mySeatID+1] == 1 {
+			break
+		} else {
+			mySeatID--
+		}
+	}
+	fmt.Println("My seat: ", mySeatID)
+}
